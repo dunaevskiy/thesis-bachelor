2af0c8f9e57f49f3d07e647b30a57f73fc094d98
Sergey Dunaevskiy <sergey@dunaevskiy.eu>
2019-04-06 19:27:31 +0200

    update: sort acronyms

--- bin/acronyms.sh
+++ bin/acronyms.sh
@@ -1,1 +1,3 @@
-gsed -E 's/^((\w|\+)+):\s+(.*)$/\\newacronym{\L\1\E}{\1}{\3}/' ./lib/acronyms.yml > ./tmp/acronyms.tex
+gsed -E 's/^((\w|\+)+):\s+(.*)$/\\newacronym{\L\1\E}{\1}{\3}/' ./lib/acronyms.yml > ./tmp/acronyms.unsorted.tex
+
+sort ./tmp/acronyms.unsorted.tex > ./tmp/acronyms.tex
